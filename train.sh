d99ebcb65f07108ed0112e8f43941fb76575987d
Danish Syed <syeddanish.ece@gmail.com>
2020-03-08 18:41:56 -0400

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,13 @@
+pip install stylegan2_pytorch
+
+INPUT_FOLDER=$1
+NAME=$2
+RESULT_DIR=$3
+IMAGE_SIZE=$4
+BATCH_SIZE=$5
+
+stylegan2_pytorch --data ${INPUT_FOLDER} \
+                  --name ${NAME} \ 
+                  --results_dir ${RESULT_DIR} \
+                  --image-size ${IMAGE_SIZE} \
+                  --batch-size ${BATCH_SIZE} \
