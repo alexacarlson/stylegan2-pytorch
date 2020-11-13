e1d4e08836700b2d9b658df39f1471fb5a8024b0
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-11-13 16:54:47 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,14 +1,15 @@
 pip install stylegan2_pytorch
 
-INPUT_FOLDER=$1
-NAME=$2
+NAME=$1
+INPUT_FOLDER=$2
 RESULT_DIR=$3
-IMAGE_SIZE=$4
-BATCH_SIZE=$5
+MODEL_DIR=$4
+IMAGE_SIZE=$5
+BATCH_SIZE=$6
 
 stylegan2_pytorch --data ${INPUT_FOLDER} \
                   --name ${NAME} \ 
                   --results_dir ${RESULT_DIR} \
-                  --models_dir ${RESULT_DIR} \
+                  --models_dir ${MODEL_DIR} \
                   --image-size ${IMAGE_SIZE} \
                   --batch-size ${BATCH_SIZE} \
