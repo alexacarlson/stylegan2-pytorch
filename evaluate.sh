bb6747e70360387357e1f7b9c25b7c315618710f
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2021-01-14 11:27:33 -0500

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -2,6 +2,7 @@
 #pip install stylegan2_pytorch
 pip install https://github.com/alexacarlson/stylegan2-pytorch/archive/master.zip
 
-CHECKPOINT_DIR=$1
+RESULTS_DIR=$1
+MODEL_DIR=$2
 
-stylegan2_pytorch --generate ${CHECKPOINT_DIR} 
+stylegan2_pytorch --generate --results_dir=${RESULT_DIR} --models_dir=${MODEL_DIR}
