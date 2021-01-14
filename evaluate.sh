ce20d923488f9c22352ed57f1eb5e93f852815a8
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2021-01-14 11:19:35 -0500

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,5 +1,7 @@
 
-pip install stylegan2_pytorch
+#pip install stylegan2_pytorch
+pip install https://github.com/alexacarlson/stylegan2-pytorch/archive/master.zip
+
 CHECKPOINT_DIR=$1
 
 stylegan2_pytorch --generate ${CHECKPOINT_DIR} 
