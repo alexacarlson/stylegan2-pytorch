7b87b32b20267dea98f9cc3184eb48415398de03
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2021-01-14 11:00:37 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,14 +5,7 @@ NAME=$1
 INPUT_FOLDER=$2
 RESULT_DIR=$3
 MODEL_DIR=$4
-IMAGE_SIZE=$5
-BATCH_SIZE=$6
+IMAGESIZE=$5
+BATCHSIZE=$6
 
-echo ${INPUT_FOLDER} ${NAME} ${RESULT_DIR} ${MODEL_DIR} ${IMAGE_SIZE} ${BATCH_SIZE} 
-
-stylegan2_pytorch --data=${INPUT_FOLDER} \
-                  --name=${NAME} \ 
-                  --results_dir=${RESULT_DIR} \
-                  --models_dir=${MODEL_DIR} \
-                  --image-size=${IMAGE_SIZE} \
-                  --batch-size=${BATCH_SIZE} 
+stylegan2_pytorch --data=${INPUT_FOLDER} --name=${NAME} --results_dir=${RESULT_DIR} --models_dir=${MODEL_DIR} --image-size=${IMAGESIZE} --batch-size=${BATCHSIZE} 
